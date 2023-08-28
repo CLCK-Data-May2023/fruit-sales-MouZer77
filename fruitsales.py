cca80d80e32daf0a687586864214b2e11342e0df
MouZer77 <skymouser@gmail.com>
2023-08-28 12:17:44 -0400

    Data Frame Exercises

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,7 @@
-# add your code here
+import pandas as pd
+
+fruit_df = pd.DataFrame({'Apples': ['35', '41'], 'Bananas': ['21', '34']})
+fruit_df.index = ["2017 Sales", "2018 Sales"]
+fruit_df
+
+fruit_df.to_csv('fruit.csv', index=True)
